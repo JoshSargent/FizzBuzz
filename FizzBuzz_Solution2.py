19d81fc8c12731e3169731542d6fcb1bd88e29e9
Josh Sargent <139649467+JoshSargent@users.noreply.github.com>
2023-12-01 12:11:15 +0000

    Added additional comments to FizzBuzz_Solution2.py

--- FizzBuzz_Solution2.py
+++ FizzBuzz_Solution2.py
@@ -1,3 +1,4 @@
+# FIZZBUZZ SOLUTION_2 #
 output = ""  # Creates empty output string.
 for num in range(1, 101):  # Defines range of 1-101.
     if num % 3 == 0:  # Checks if number is divisible by 3.
